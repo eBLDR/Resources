5216c9573f4dfb5103bf46fc7e9ed7a633c2d6e3
eBLDR <ed.bldr@gmail.com>
2019-12-09 11:29:13 +0100

    Service orchestration with docker compose

--- DockerComposeApplication/website/app.py
+++ DockerComposeApplication/website/app.py
@@ -1,28 +1,48 @@
-from requests import get
 from flask import Flask
+from requests import get
 
 app = Flask(__name__)
-
-# Docker-compose created a virtual network among the container, their host
-# name matches their service name.
-PRODUCTS_API_URL = 'http://products-service'
 
 
 @app.route('/')
 def index():
-    return 'Welcome to shop!'
+    return '''
+    <html>
+        <head>
+            <title>Little Shop</title>
+        </head>
+        <body>
+            <h1>Welcome!</h1>
+            <h2>Available:</h2>
+            <ul>
+                {items_list}
+            </ul>
+        </body>
+    </html>'''.format(
+        items_list=''.join([
+            f'<li>{item.title()}: {qty}</li>'
+            for item, qty in ProductsServiceClient().get_products().items()
+        ])
+    )
 
 
-@app.route('/products')
-def products():
+# Following code would usually be in another file
+class ProductsServiceClient:
     """
     This app will communicate with products API.
     """
-    # Make a request to products service
-    try:
-        return get(PRODUCTS_API_URL).json()
-    except ConnectionError:
-        return 'Products service is not available.'
+
+    def __init__(self):
+        # Docker-compose created a virtual network among the container, their host
+        # name matches their service name.
+        self.products_api_url = 'http://products-service'
+
+    def get_products(self):
+        # Make a request to products service
+        try:
+            return get(self.products_api_url).json()
+        except ConnectionError:
+            return 'Products service is not available.'
 
 
 if __name__ == '__main__':
